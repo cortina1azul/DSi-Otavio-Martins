cb298237125673a0387b779df12f1e8e8b2ba76f
Otavio <c0rt1n4.azul@gmail.com>
2026-03-16 09:23:45 -0300

    AULA DIA 16/03

---
+++ aula02/Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+     public static void main(String[] args) {
+          String mensagempt1 = "Meu pokémon favorito é o... ";
+          String mensagempt2 = "Vaporeon";
+          System.out.println(mensagempt1.concat(mensagempt2));
+
+          int n1 = 1;
+          String n2 = "2";
+          System.out.print(n1 + n2);
+        }      
+}
