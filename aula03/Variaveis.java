949d0a59203c20b2a429d8355ff347268c4db090
Otavio <c0rt1n4.azul@gmail.com>
2026-04-06 08:37:29 -0300

    aula 06/04

---
+++ aula03/Variaveis.java
@@ -0,0 +1,9 @@
+public class Variaveis {
+    public static void main(String [] args) {
+        int myNum;
+        myNum = 67;
+        String msgTeste;
+        msgTeste = " Six Seven";
+        System.out.println(myNum + msgTeste);
+    }
+}
