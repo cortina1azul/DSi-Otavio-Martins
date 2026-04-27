d201c2e4a0af225d7ece92d091139c815e849ebe
Otavio <c0rt1n4.azul@gmail.com>
2026-04-27 09:31:22 -0300

    Operadores matematicos

--- aula05/MatematicaJava.java
+++ aula05/MatematicaJava.java
@@ -1,5 +1,8 @@
 public class MatematicaJava {
     public static void main(String[] args) {
+
+      System.out.println("\\\\ Operações matemáticas \\\\");
+
       int x = 670;
       int y = 67;
   
@@ -14,6 +17,20 @@ public class MatematicaJava {
       System.out.println(z); // 69
       --z;
       System.out.println(z); // 67
+
+      System.out.println("\\\\ Diferenças de inteiros e decimais na divisão Java \\\\");
+
+      int a = 34;
+      int b = 11;
+      System.out.println(a / b);   // Integer division, result is 3
+      
+      
+
+      double c = 11.0d;
+      double d = 34.0d;
+      System.out.println(c / d);   // Decimal division, result is 3.333...
+
+
     }
   }
   
