599f13105306f8880f02cb63f34cf2c538f2e951
Otavio <c0rt1n4.azul@gmail.com>
2026-05-04 09:27:47 -0300

    aula6

---
+++ aula06/Comparacoes.java
@@ -0,0 +1,14 @@
+public class Comparacoes {
+  public static void main(String[] args) {
+   String Password = "JoaoSales67_Sigma_Aurudo_FarmadorDeAura_Moggador"
+   String HackerPassword = "JoaoSiles"
+
+   if (HackerPassword == Password) {
+        System.out.println("Acesso concedido!")
+   } else {
+        System.out.println("DESTRUINDO O COMPUTADOR. Delete User/System32")
+   }
+
+  }
+}
+
